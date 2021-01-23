221c5b5aadb047fe1a4f25fd9ff2e6366ab03364
JoshLinneburg <joshualinneburg@gmail.com>
2021-01-23 00:20:45 -0500

    Added docstring to main.py

--- src/core/main.py
+++ src/core/main.py
@@ -8,13 +8,48 @@ from src.core.utils import (
 
 
 def main(
-    aws_profile_name,
     root_path,
     recursive,
     bucket_name,
+    aws_profile_name=None,
     key_prefix=None,
     extensions=None,
 ):
+    """
+    Main program entrypoint - runs the S3 video upload program.
+
+    Parameters
+    ----------
+    root_path: str
+        Root path where the files you want to upload to S3 are.
+
+    recursive: bool
+        Whether or not you want the program to recursively find files in subdirectories.
+
+    bucket_name: str
+        Name of the S3 bucket to upload to.
+
+    aws_profile_name: str (Optional)
+        The name of the AWS profile to use - this will look in the ~/.aws/credentials
+        file on your machine and use the credentials provided under the "aws_profile_name"
+        entry.
+
+        If running on your personal machine, you must specify this parameter.
+
+    key_prefix: str (Optional)
+        The key prefix of the files you wish to upload. If you do not specify this,
+        the files will be uploaded using the absolute path from your computer.
+
+        E.g., if this is not passed in, your files will be located as C:/Users/path/to/files/
+        in the S3 bucket.
+
+    extensions: tuple (Optional)
+        Valid file extensions to be uploaded.
+
+    Returns
+    -------
+    None
+    """
     client = init_aws_client(
         service_name="s3", profile_name=aws_profile_name, region_name="us-east-1"
     )
